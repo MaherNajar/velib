b06ed6fcee58d3b40c59c24b1bf588498983b534
maher najar <mahernajar87@gmail.com>
2018-03-12 11:33:00 +0100

    Code review

--- project.js
+++ project.js
@@ -20,7 +20,7 @@ function initMap() {
 				var stationName = e.fields.name;
 				var numbikesavailable = e.fields.numbikesavailable;
 				var capacity = e.fields.capacity;
-				var recordTime = new Date(e.record_timestamp);	
+				var recordTime = new Date(e.record_timestamp);
 
 				var marker = new google.maps.Marker({
 					position: { lat: lattitude, lng: longitude },
@@ -47,57 +47,50 @@ puis traite la réponse */
 $("form").submit(function (e) {
 
 	e.preventDefault();
-  
-  
-	var $target = $('html,body');
-	var $modalWindow = $('#modalWindow');
-	var $footer = $('#footer');
-  
+
 	var $form = $(this);
 	var $station = $('#name').html();
-	var $nom = $('#nom').val();
-	var $email = $('#email').val();
-	var $time = $('#duration').val();
+	var $nom = f.nom.value;
+	var $email = f.email.value;
+	var $time = f.duration.value;
 	var $duration = $time.substr(0, 2) + ' heures et ' + $time.substr(3, 5) + ' minutes';
-	var $nameStation = $('#nameStation');
-	var $rentaltime = $('#rentaltime');
-	$nameStation.val($station);
-	$rentaltime.val($duration);
+
+	$('#nameStation').val($station);
+	$('#rentaltime').val($duration);
+
 	sessionStorage.setItem("nom", $nom);
 	sessionStorage.setItem("email", $email);
 	sessionStorage.setItem("duree", $duration);
 	sessionStorage.setItem("station", $station);
-  
-  
+
+
 	$.post($form.attr("action"), $form.serialize())
-  
-	  .done(function (data) {
-		$footer.html(data);
-		$modalWindow.modal("hide");
-		$target.animate({ scrollTop: $footer.height() }, 1000);
-	  })
-  
-	  .fail(function () {
-		alert('ça marche pas..');
-		$footer.html($nom + ' - ' + $email + ' a réservé 1 vélo pour ' + $duration + ' à la station ' + $station);
-		$modalWindow.modal("hide");
-		$target.animate({ scrollTop: $footer.height() }, 1000);
-	  });
-  });
-  
-  // Affiche les variables de sessions 
-  function session() {
-  
-	var $target = $('html,body');
-	var $footer = $('#footer');
+
+		.done(function (data) {
+			$('#footer').html(data);
+			$('#modalWindow').modal("hide");
+			$('html,body').animate({ scrollTop: $('#footer').height() }, 1000);
+		})
+
+		.fail(function () {
+			alert('ça marche pas..');
+			$('#footer').html($nom + ' - ' + $email + ' a réservé 1 vélo pour ' + $duration + ' à la station ' + $station);
+			$('#modalWindow').modal("hide");
+			$('html,body').animate({ scrollTop: $('#footer').height() }, 1000);
+		});
+});
+
+// Affiche les variables de sessions 
+function session() {
+
 	var nom = sessionStorage.getItem('nom');
 	var email = sessionStorage.getItem('email');
 	var duree = sessionStorage.getItem('duree');
 	var station = sessionStorage.getItem('station');
-  
-	$footer.html(nom + ' - ' + email + ' a reservé 1 vélo pour ' + duree + ' à la station ' + station);
-	$target.animate({ scrollTop: $footer.height() }, 1000);
-  
-	if ($footer.html() == 'null - null a reservé 1 vélo pour null à la station null')
-	  $footer.html('');
-  };
+
+	$('#footer').html(nom + ' - ' + email + ' a reservé 1 vélo pour ' + duree + ' à la station ' + station);
+	$('html,body').animate({ scrollTop: $('#footer').height() }, 1000);
+
+	if ($('#footer').html() == 'null - null a reservé 1 vélo pour null à la station null')
+		$('#footer').html('');
+};
