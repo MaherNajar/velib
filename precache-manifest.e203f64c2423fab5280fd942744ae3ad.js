22d79f70fa3a8fb7f6cadcfc8e3d96b0ce6845ef
maher <mahernajar87@gmail.com>
2019-08-13 23:19:09 +0200

    Updates

---
+++ precache-manifest.e203f64c2423fab5280fd942744ae3ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b38510a652ec278a0b960245fdee2a4",
+    "url": "/velib/index.html"
+  },
+  {
+    "revision": "2cade1c1f378630595b8",
+    "url": "/velib/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9e2929574b874970909a",
+    "url": "/velib/static/css/main.b35dedef.chunk.css"
+  },
+  {
+    "revision": "2cade1c1f378630595b8",
+    "url": "/velib/static/js/2.788e6620.chunk.js"
+  },
+  {
+    "revision": "9e2929574b874970909a",
+    "url": "/velib/static/js/main.1e065325.chunk.js"
+  },
+  {
+    "revision": "a7e3eb621309d52477cb",
+    "url": "/velib/static/js/runtime~main.7a4e9bb8.js"
+  }
+]);
